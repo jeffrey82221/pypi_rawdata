26a01e2b1a2653f4167b97337d99f4dc706d5f12
jeffrey82221 <jeffrey82221@gmail.com>
2024-05-01 23:21:03 +0800

    alter: path

--- src/get_dep.sh
+++ src/get_dep.sh
@@ -1,3 +1,3 @@
 echo 'download' $1 '...'
-python3 -m pip install --ignore-installed --dry-run $1 --report data/pipdeptree/$1.json --quiet
+python3 -m pip install --ignore-installed --dry-run $1 --report pipdeptree/$1.json --quiet
 echo $1 'finish'
