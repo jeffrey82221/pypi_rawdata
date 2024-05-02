05717acd7148273866459343f581065a25be7bb6
jeffrey82221 <jeffrey82221@gmail.com>
2024-05-02 21:49:37 +0800

    tune: worker count

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from src import SimplePyPiCanonicalize
 from batch_framework.filesystem import DropboxBackend
-
-
 etl_obj = SimplePyPiCanonicalize(
     raw_df=DropboxBackend('/data/canon/raw/'),
     tmp_fs=DropboxBackend('/data/canon/tmp/'),
     output_fs=DropboxBackend('/data/canon/output/'),
     partition_fs=DropboxBackend('/data/canon/partition/'),
-    download_worker_count=32 * 8,
+    download_worker_count=2,
     update_worker_count=32,
-    test_count=8192 * 8,
+    test_count=8192,
     do_update=True
 )
-
 if __name__ == '__main__':
     etl_obj.execute()
+    
