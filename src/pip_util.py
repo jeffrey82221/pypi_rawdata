cb159cb153f2a2d18e02b9804b70e53e2df5f1f0
jeffrey82221 <jeffrey82221@gmail.com>
2024-05-04 23:18:41 +0800

    feature: enhance requirements

--- src/pip_util.py
+++ src/pip_util.py
@@ -3,20 +3,32 @@ Convert requires_dist to
 mapping of required_package to suitable versions
 """
 from typing import List, Optional, Tuple, Dict
-from pip._vendor.distlib.util import parse_requirement
+from pip._vendor.distlib.util import parse_requirement, parse_name_and_version
 from pip._vendor.distlib.locators import PyPIJSONLocator
 from pip._vendor.distlib import DistlibException
 
 def get_dist(dist_str) -> Tuple[str, List[str]]:
     try:
-        name = parse_requirement(dist_str).name
+        name_space = parse_requirement(dist_str)
+        name = name_space.name
         dists = PyPIJSONLocator('https://pypi.org/pypi/').get_project(name)
         versions = [key for key in dists if key not in ['urls', 'digests']]
-        results = []
+        releases = []
         for version in versions:
-            if dists[version].matches_requirement(dist_str):
-                results.append(version)
-        return (name, results)
+            try:
+                if dists[version].matches_requirement(dist_str):
+                    releases.append(version)
+            except BaseException:
+                pass
+        data = {
+            'releases': releases,
+            'extras': name_space.extras,
+            'constraints': name_space.constraints,
+            'marker': name_space.marker,
+            'url': name_space.url,
+            'requirement': name_space.requirement
+        }
+        return (name, data)
     except (DistlibException, SyntaxError):
         return (dist_str, None)
 
